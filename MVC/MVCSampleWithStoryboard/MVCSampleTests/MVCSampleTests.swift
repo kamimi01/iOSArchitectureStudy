12e7ec0f941c4ab7d43414af8a9f0be7083db3bb
kamimi01 <awakaru0122@gmail.com>
2021-05-19 23:33:58 +0900

    feat: Modelのテストを書いた

--- MVC/MVCSampleWithStoryboard/MVCSampleTests/MVCSampleTests.swift
+++ MVC/MVCSampleWithStoryboard/MVCSampleTests/MVCSampleTests.swift
@@ -11,16 +11,59 @@ import XCTest
 class MVCSampleTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // 何もしない
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_カウントがマイナス1される() throws {
+        // Arrange
+        let model = Model()
+        
+        // Act
+        model.countDown()
+        
+        // Assert
+        XCTAssertEqual(model.count, -1)
+    }
+    
+    func test_カウントがマイナス10される() throws {
+        // Arrange
+        let model = Model()
+        
+        // Act
+        for _ in 1..<11 {
+            model.countDown()
+        }
+        
+        // Assert
+        XCTAssertEqual(model.count, -10)
+    }
+    
+    func test_カウントがプラス1される() throws {
+        // Arrange
+        let model = Model()
+        
+        // Act
+        model.countUp()
+        
+        // Assert
+        XCTAssertEqual(model.count, 1)
+    }
+    
+    func test_カウントがプラス10される() throws {
+        // Arrange
+        let model = Model()
+        
+        // Act
+        for _ in 1..<11 {
+            model.countUp()
+        }
+        
+        // Assert
+        XCTAssertEqual(model.count, 10)
     }
 
     func testPerformanceExample() throws {
