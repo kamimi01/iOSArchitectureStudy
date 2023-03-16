55dfa163bbeecf738bc8f455bdf29b0b12d3e7d4
mika.urakawa <awakaru0122@gmail.com>
2023-03-17 06:55:37 +0900

    feat: FLUX sample initiation

---
+++ FLUX/FLUXSample/FLUXSample/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  FLUXSample
+//
+//  Created by mikaurakawa on 2023/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let searchField = UISearchBar()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let screenWidth = self.view.frame.width
+        let screenHeight = self.view.frame.height
+
+        searchField.frame = CGRect(x: 0, y: screenHeight / 4, width: screenWidth, height: 50)
+        searchField.delegate = self
+
+        self.view.addSubview(searchField)
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("検索ボタンが押された！")
+    }
+
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        print("テキストフィールド入力開始")
+        return true
+    }
+}
