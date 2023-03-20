1ec48514b5baecd770d4947de99b85fd4dcc34c5
mika.urakawa <awakaru0122@gmail.com>
2023-03-21 06:54:09 +0900

    feat: add dispatcher

---
+++ FLUX/FLUXSample/FLUXSample/Flux/Dispatcher.swift
@@ -0,0 +1,44 @@
+//
+//  Dispatcher.swift
+//  FLUXSample
+//
+//  Created by mikaurakawa on 2023/03/21.
+//
+
+import Foundation
+
+typealias DispatchToken = String
+
+class Dispatcher {
+    static let shared = Dispatcher()
+
+    let lock: NSLocking  // 複数のスレッドからAction がDispatchされる可能性があるので、排他制御が必要
+    private var callbacks: [DispatchToken: (Action) -> ()]
+
+    init() {
+        self.lock = NSRecursiveLock()
+        self.callbacks = [:]
+    }
+
+    func register(callback: @escaping (Action) -> ()) -> DispatchToken {
+        lock.lock(); defer { lock.unlock() }
+
+        let token = UUID().uuidString
+        callbacks[token] = callback
+        return token  // 登録を解除するためのトークン
+    }
+
+    func unregister(_ token: DispatchToken) {
+        lock.lock(); defer { lock.unlock() }
+
+        callbacks.removeValue(forKey: token)
+    }
+
+    func dispatch(_ action: Action) {
+        lock.lock(); defer { lock.unlock() }
+
+        callbacks.forEach { _, callback in
+            callback(action)
+        }
+    }
+}
